df7bfc2de611c2ebe836dbaa7ccb23bb1096d5ef
Ian Engelbrecht <ianicus.za@gmail.com>
2025-09-25 16:07:48 +0200

    added infowindows

--- src/routes/results/+page.svelte
+++ src/routes/results/+page.svelte
@@ -13,7 +13,7 @@
 		});
 
 		loader.load().then(async () => {
-			const { Map } = await google.maps.importLibrary('maps');
+			const { Map, InfoWindow } = await google.maps.importLibrary('maps');
 			const { AdvancedMarkerElement } = await google.maps.importLibrary('marker');
 
 			const map = new Map(mapElement, {
@@ -24,10 +24,37 @@
 
 			results.forEach((result) => {
 				if (!result.coords) return;
+
 				const marker = new AdvancedMarkerElement({
 					map: map,
 					position: { lat: result.coords.latitude, lng: result.coords.longitude },
 					title: result.kbps.toFixed(2) + ' kbps'
+				});
+
+				// Create the info window content
+				const infoWindow = new google.maps.InfoWindow({
+					content: `
+            <div style="min-width: 120px;">
+              <strong>Time:</strong> ${result.timestamp ? new Date(result.timestamp).toLocaleString() : 'N/A'}<br>
+              <strong>Provider:</strong> ${result.provider ? result.provider : 'N/A'}<br>
+              <strong>Passes:</strong> ${result.passes}<br>
+              <strong>Speed:</strong> ${result.kbps.toFixed(2)} kbps<br>
+              <strong>AveMs:</strong> ${result.avgMs ? result.avgMs.toFixed(2) : 'N/A'} ms<br>
+              <strong>Downlink:</strong> ${result.connection && result.connection.downlink ? result.connection.downlink : 'N/A'}<br>
+              <strong>Type:</strong> ${result.connection && result.connection.effectiveType ? result.connection.effectiveType : 'N/A'}<br>
+              <strong>Device:</strong> ${result.userAgent ? result.userAgent.device.name : 'N/A'}<br>
+              <strong>OS:</strong> ${result.userAgent ? result.userAgent.os.name : 'N/A'}<br>
+              <strong>Browser:</strong> ${result.userAgent ? result.userAgent.browser.name : 'N/A'}<br>
+            </div>
+          `
+				});
+
+				// Add a click listener to open the info window
+				marker.addListener('click', () => {
+					infoWindow.open({
+						anchor: marker,
+						map
+					});
 				});
 			});
 		});
